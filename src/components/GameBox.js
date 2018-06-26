2850feaa9f1fab18191e622c492d7fb8388ca6ff
GLOBAL\I343436 <jason.noh@sap.com>
2018-06-26 16:48:44 +1000

    adding redux with subfolders

---
+++ src/components/GameBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GameBox extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1> this is a game box </h1>
+			</div>
+		);
+	}
+}
+
+export default GameBox;
